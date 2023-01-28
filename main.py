6f1544ad6f176e34cfc0bbae28fa67ca3f8e926a
Luiz <ireliontt@gmail.com>
2023-01-28 18:15:33 -0300

    código atual

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import Functions
 from pprint import pprint
-import re
 
 
-#pprint (Functions.open_Le())
-#pprint (Functions.open_PQ())
+Functions.open_Le()
+Functions.open_PQ()
 pprint(Functions.check())
 Functions.open_popup()
 
